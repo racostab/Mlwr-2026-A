34850a0fb5aa51addd77c56896c861176607a8f5
eduardo <lpatlang1700@alumno.ipn.mx>
2026-03-17 07:39:27 -0600

    archivos docker y vbox

--- PATLAN-GUALO-LUIS-EDUARDO/docker/ssh_client.sh
+++ PATLAN-GUALO-LUIS-EDUARDO/docker/ssh_client.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-
-
-# Leer config.init
 SSH_HOST=$(grep "SSH_HOST=" config.init | cut -d'=' -f2)
 SSH_PORT=$(grep "SSH_PORT=" config.init | cut -d'=' -f2)
 SSH_USER=$(grep "SSH_USER=" config.init | cut -d'=' -f2)
 SSH_KEY=$(grep "SSH_KEY=" config.init | cut -d'=' -f2)
-
 KEY="$SSH_KEY"
 
-# Encender el contenedor
 docker start kali
-
 chmod 600 "$KEY"
 
-# Conectarse
+echo "[+] Esperando que SSH esté disponible en $SSH_HOST:$SSH_PORT..."
+
+
+for i in $(seq 1 15); do
+    if ssh -o StrictHostKeyChecking=no \
+           -o ConnectTimeout=1 \
+           -o BatchMode=yes \
+           -i "$KEY" -p "$SSH_PORT" \
+           "$SSH_USER@$SSH_HOST" true 2>/dev/null; then
+        break
+    fi
+    sleep 1
+done
+
 echo "[+] Conectando a $SSH_USER@$SSH_HOST:$SSH_PORT"
-ssh -o StrictHostKeyChecking=no -i "$KEY" -p $SSH_PORT "$SSH_USER@$SSH_HOST"
+ssh -o StrictHostKeyChecking=no -i "$KEY" -p "$SSH_PORT" "$SSH_USER@$SSH_HOST"
